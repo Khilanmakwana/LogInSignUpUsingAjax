f25045ae3d25b10d08145ed102bb659a4686c521
Makwana Khilan <31216771+Khilanmakwana@users.noreply.github.com>
2019-03-17 16:24:47 +0530

    First commit

--- LogInSignup/Controllers/HomeController.cs
+++ LogInSignup/Controllers/HomeController.cs
@@ -3,28 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LogInSignup.Models;
 
 namespace LogInSignup.Controllers
 {
     public class HomeController : Controller
     {
+        getmecabEntities db = new getmecabEntities();
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        public JsonResult SaveData(User model)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            db.Users.Add(model);
+            db.SaveChanges();
+            return Json("Registration Successful", JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Contact()
+        public JsonResult CheckValidUser(User model)
         {
-            ViewBag.Message = "Your contact page.";
+            string result = "Fail";
+            var DataItem = db.Users.Where(x => x.Email == model.Email && x.Password == model.Password).SingleOrDefault();
+            if (DataItem != null)
+            {
+                Session["UserId"] = DataItem.Id.ToString();
+                Session["UserName"] = DataItem.Email.ToString();
+                result = "Success";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
 
-            return View();
+        public ActionResult AfterLogin()
+        {
+            if (Session["UserId"] != null)
+            {
+                return View();
+            }
+            else {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Index");
         }
     }
 }
